49d865a0b15d15d37e9025b6c753d24aff439a31
Kevin Dunetz <kdunetz@us.ibm.com>
2018-03-16 11:38:31 -0400

    fix stuff

--- create_pv_directories.sh
+++ create_pv_directories.sh
@@ -1,61 +1,61 @@
 mkdir /storage
-mkdir /storage/vol60-1gi
- chmod 0777 /storage/vol60-1gi
-mkdir /storage/vol61-1gi
- chmod 0777 /storage/vol61-1gi
-mkdir /storage/vol62-1gi
- chmod 0777 /storage/vol62-1gi
-mkdir /storage/vol63-1gi
- chmod 0777 /storage/vol63-1gi
-mkdir /storage/vol64-1gi
- chmod 0777 /storage/vol64-1gi
-mkdir /storage/vol65-1gi
- chmod 0777 /storage/vol65-1gi
-mkdir /storage/vol66-1gi
- chmod 0777 /storage/vol66-1gi
-mkdir /storage/vol67-1gi
- chmod 0777 /storage/vol67-1gi
-mkdir /storage/vol68-1gi
- chmod 0777 /storage/vol68-1gi
-mkdir /storage/vol69-1gi
- chmod 0777 /storage/vol69-1gi
-mkdir /storage/vol70-5gi
- chmod 0777 /storage/vol70-5gi
-mkdir /storage/vol71-5gi
- chmod 0777 /storage/vol71-5gi
-mkdir /storage/vol72-5gi
- chmod 0777 /storage/vol72-5gi
-mkdir /storage/vol73-5gi
- chmod 0777 /storage/vol73-5gi
-mkdir /storage/vol74-5gi
- chmod 0777 /storage/vol74-5gi
-mkdir /storage/vol75-10gi
- chmod 0777 /storage/vol75-10gi
-mkdir /storage/vol76-10gi
- chmod 0777 /storage/vol76-10gi
-mkdir /storage/vol77-10gi
- chmod 0777 /storage/vol77-10gi
-mkdir /storage/vol78-10gi
- chmod 0777 /storage/vol78-10gi
-mkdir /storage/vol79-10gi
- chmod 0777 /storage/vol79-10gi
-mkdir /storage/vol80-20gi
- chmod 0777 /storage/vol80-20gi
-mkdir /storage/vol81-20gi
- chmod 0777 /storage/vol81-20gi
-mkdir /storage/vol82-20gi
- chmod 0777 /storage/vol82-20gi
-mkdir /storage/vol83-20gi
- chmod 0777 /storage/vol83-20gi
-mkdir /storage/vol84-20gi
- chmod 0777 /storage/vol84-20gi
-mkdir /storage/vol85-50gi
- chmod 0777 /storage/vol85-50gi
-mkdir /storage/vol86-50gi
- chmod 0777 /storage/vol86-50gi
-mkdir /storage/vol87-50gi
- chmod 0777 /storage/vol87-50gi
-mkdir /storage/vol88-50gi
- chmod 0777 /storage/vol88-50gi
-mkdir /storage/vol89-50gi
- chmod 0777 /storage/vol89-50gi
+mkdir /storage/vol01-1gi
+chmod 0777 /storage/vol01-1gi
+mkdir /storage/vol02-1gi
+chmod 0777 /storage/vol02-1gi
+mkdir /storage/vol03-1gi
+chmod 0777 /storage/vol03-1gi
+mkdir /storage/vol04-1gi
+chmod 0777 /storage/vol04-1gi
+mkdir /storage/vol05-1gi
+chmod 0777 /storage/vol05-1gi
+mkdir /storage/vol06-1gi
+chmod 0777 /storage/vol06-1gi
+mkdir /storage/vol07-1gi
+chmod 0777 /storage/vol07-1gi
+mkdir /storage/vol08-1gi
+chmod 0777 /storage/vol08-1gi
+mkdir /storage/vol09-1gi
+chmod 0777 /storage/vol09-1gi
+mkdir /storage/vol10-1gi
+chmod 0777 /storage/vol10-1gi
+mkdir /storage/vol11-5gi
+chmod 0777 /storage/vol11-5gi
+mkdir /storage/vol12-5gi
+chmod 0777 /storage/vol12-5gi
+mkdir /storage/vol13-5gi
+chmod 0777 /storage/vol13-5gi
+mkdir /storage/vol14-5gi
+chmod 0777 /storage/vol14-5gi
+mkdir /storage/vol15-5gi
+chmod 0777 /storage/vol15-5gi
+mkdir /storage/vol16-10gi
+chmod 0777 /storage/vol16-10gi
+mkdir /storage/vol17-10gi
+chmod 0777 /storage/vol17-10gi
+mkdir /storage/vol18-10gi
+chmod 0777 /storage/vol18-10gi
+mkdir /storage/vol19-10gi
+chmod 0777 /storage/vol19-10gi
+mkdir /storage/vol20-10gi
+chmod 0777 /storage/vol20-10gi
+mkdir /storage/vol21-20gi
+chmod 0777 /storage/vol21-20gi
+mkdir /storage/vol22-20gi
+chmod 0777 /storage/vol22-20gi
+mkdir /storage/vol23-20gi
+chmod 0777 /storage/vol23-20gi
+mkdir /storage/vol24-20gi
+chmod 0777 /storage/vol24-20gi
+mkdir /storage/vol25-20gi
+chmod 0777 /storage/vol25-20gi
+mkdir /storage/vol26-50gi
+chmod 0777 /storage/vol26-50gi
+mkdir /storage/vol27-50gi
+chmod 0777 /storage/vol27-50gi
+mkdir /storage/vol28-50gi
+chmod 0777 /storage/vol28-50gi
+mkdir /storage/vol29-50gi
+chmod 0777 /storage/vol29-50gi
+mkdir /storage/vol30-50gi
+chmod 0777 /storage/vol30-50gi
