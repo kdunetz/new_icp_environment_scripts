b8c54679227c8b09e733129261d2b3883d1bd8ef
Kevin Dunetz <kdunetz@us.ibm.com>
2018-03-16 11:29:57 -0400

    first commit

---
+++ create_pv_directories.sh
@@ -0,0 +1,60 @@
+mk /storage/vol60-1gi
+ chmod 0777 /storage/vol60-1gi
+mk /storage/vol61-1gi
+ chmod 0777 /storage/vol61-1gi
+mk /storage/vol62-1gi
+ chmod 0777 /storage/vol62-1gi
+mk /storage/vol63-1gi
+ chmod 0777 /storage/vol63-1gi
+mk /storage/vol64-1gi
+ chmod 0777 /storage/vol64-1gi
+mk /storage/vol65-1gi
+ chmod 0777 /storage/vol65-1gi
+mk /storage/vol66-1gi
+ chmod 0777 /storage/vol66-1gi
+mk /storage/vol67-1gi
+ chmod 0777 /storage/vol67-1gi
+mk /storage/vol68-1gi
+ chmod 0777 /storage/vol68-1gi
+mk /storage/vol69-1gi
+ chmod 0777 /storage/vol69-1gi
+mk /storage/vol70-5gi
+ chmod 0777 /storage/vol70-5gi
+mk /storage/vol71-5gi
+ chmod 0777 /storage/vol71-5gi
+mk /storage/vol72-5gi
+ chmod 0777 /storage/vol72-5gi
+mk /storage/vol73-5gi
+ chmod 0777 /storage/vol73-5gi
+mk /storage/vol74-5gi
+ chmod 0777 /storage/vol74-5gi
+mk /storage/vol75-10gi
+ chmod 0777 /storage/vol75-10gi
+mk /storage/vol76-10gi
+ chmod 0777 /storage/vol76-10gi
+mk /storage/vol77-10gi
+ chmod 0777 /storage/vol77-10gi
+mk /storage/vol78-10gi
+ chmod 0777 /storage/vol78-10gi
+mk /storage/vol79-10gi
+ chmod 0777 /storage/vol79-10gi
+mk /storage/vol80-20gi
+ chmod 0777 /storage/vol80-20gi
+mk /storage/vol81-20gi
+ chmod 0777 /storage/vol81-20gi
+mk /storage/vol82-20gi
+ chmod 0777 /storage/vol82-20gi
+mk /storage/vol83-20gi
+ chmod 0777 /storage/vol83-20gi
+mk /storage/vol84-20gi
+ chmod 0777 /storage/vol84-20gi
+mk /storage/vol85-50gi
+ chmod 0777 /storage/vol85-50gi
+mk /storage/vol86-50gi
+ chmod 0777 /storage/vol86-50gi
+mk /storage/vol87-50gi
+ chmod 0777 /storage/vol87-50gi
+mk /storage/vol88-50gi
+ chmod 0777 /storage/vol88-50gi
+mk /storage/vol89-50gi
+ chmod 0777 /storage/vol89-50gi
