cb53756ab7ce57384f100753538e4daded86866a
Kevin Dunetz <kdunetz@us.ibm.com>
2018-03-23 09:15:38 -0400

    added more scripts in there

--- install_ldap.sh
+++ install_ldap.sh
@@ -10,8 +10,11 @@ export LDAP_NAME=ldap-service
 export LDAPADMIN_NAME=ldapadmin-service
 export LDAP_PORT=6389
 export LDAPADMIN_PORT=6443
+LINK_HOST=ldap-host
 
 docker pull osixia/openldap:1.1.11
 docker pull osixia/phpldapadmin:0.7.1
 
 docker run -v /opt/ldap/var/lib/ldap:/var/lib/ldap -v /opt/ldap/etc/ldap/slapd.d:/etc/ldap/slapd.d -p $LDAP_PORT:389 --name "$LDAP_NAME" --hostname "$LDAP_NAME" --env LDAP_ORGANIZATION="$ORG" --env LDAP_DOMAIN="$DOMAIN" --env LDAP_ADMIN_PASSWORD="$ADMIN_PWD" --hostname "$LDAP_NAME" --detach osixia/openldap:1.1.11
+
+docker run --name "$LDAPADMIN_NAME" --hostname "$LDAPADMIN_NAME" -p $LDAPADMIN_PORT:443 --link "$LDAP_NAME:$LINK_HOST" --env PHPLDAPADMIN_LDAP_HOSTS="$LINK_HOST" --detach osixia/phpldapadmin:0.7.1
