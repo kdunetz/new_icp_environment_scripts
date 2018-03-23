edefcbeb41e16eca8f8a28a63c6f967dd7082282
Kevin Dunetz <kdunetz@us.ibm.com>
2018-03-23 09:09:10 -0400

    Added LDAP configuration script and sample LDAP import file

---
+++ install_ldap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p /opt/ldap/var/lib/ldap
+mkdir -p /opt/ldap/etc/ldap/slapd.d
+
+export ORG=IBM
+export DOMAIN=ibm.com
+export ADMIN_PWD=Passw0rd!
+export LDAP_NAME=ldap-service
+export LDAPADMIN_NAME=ldapadmin-service
+export LDAP_PORT=6389
+export LDAPADMIN_PORT=6443
+
+docker pull osixia/openldap:1.1.11
+docker pull osixia/phpldapadmin:0.7.1
+
+docker run -v /opt/ldap/var/lib/ldap:/var/lib/ldap -v /opt/ldap/etc/ldap/slapd.d:/etc/ldap/slapd.d -p $LDAP_PORT:389 --name "$LDAP_NAME" --hostname "$LDAP_NAME" --env LDAP_ORGANIZATION="$ORG" --env LDAP_DOMAIN="$DOMAIN" --env LDAP_ADMIN_PASSWORD="$ADMIN_PWD" --hostname "$LDAP_NAME" --detach osixia/openldap:1.1.11
